c4a98783b7321347dd94e809c0449aa3c147e768
Mateus <s.mateussales@gmail.com>
2021-09-20 03:10:46 -0300

    adicionando Adapter

---
+++ app/src/main/java/com/mateus/viagens/DAO/PacoteDAO.java
@@ -0,0 +1,22 @@
+package com.mateus.viagens.DAO;
+
+import com.mateus.viagens.ui.activity.model.Pacote;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class PacoteDAO {
+    public List<Pacote>lista(){
+        List<Pacote> pacotes = new ArrayList<>(Arrays.asList(
+            new Pacote("São Paulo", "sao_paulo_sp", 2, new BigDecimal(243.99)),
+            new Pacote("Belo Horizonte", "belo_horizonte_mg", 3, new BigDecimal(421.99)),
+            new Pacote("Recife", "recife_pe", 4, new BigDecimal(754.99)),
+            new Pacote("Rio de Janeiro", "rio_de_janeiro_rj", 6, new BigDecimal(532.99)),
+            new Pacote("Salvador", "salvador_ba", 5, new BigDecimal(899.99)),
+            new Pacote("Foz do Iguaçu", "foz_do_iguacu_pr", 1, new BigDecimal(230.99))));
+
+    return pacotes;
+    }
+}
--- app/src/main/java/com/mateus/viagens/ui/activity/ListaPacotesActivity.java
+++ app/src/main/java/com/mateus/viagens/ui/activity/ListaPacotesActivity.java
@@ -3,8 +3,14 @@ package com.mateus.viagens.ui.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.mateus.viagens.DAO.PacoteDAO;
 import com.mateus.viagens.R;
+import com.mateus.viagens.ui.activity.model.Pacote;
+import com.mateus.viagens.ui.adapter.ListaPacotesAdapter;
+
+import java.util.List;
 
 public class ListaPacotesActivity extends AppCompatActivity {
 
@@ -12,5 +18,10 @@ public class ListaPacotesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_pacotes);
+        ListView listaDePacotes = findViewById(R.id.lista_pacotes_listview);
+
+        List<Pacote> pacotes = new PacoteDAO().lista();
+        listaDePacotes.setAdapter(new ListaPacotesAdapter(pacotes, this
+        ));
     }
 }
